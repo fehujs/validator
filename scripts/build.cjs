2fc6a61df087611e7c6404ee9f72a8787d9d3dfa
Pascal LLEONART <pascal.lleonart@laposte.net>
2024-12-21 11:05:09 +0100

    fix(build): add @fehujs/build

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,18 +1,4 @@
-const esbuild = require("esbuild")
-const { existsSync, rmSync } = require("node:fs")
+const build = require("@fehujs/build")
+const pkgJson = require("../package.json")
 
-
-if (existsSync('build'))
-    rmSync('build', { recursive: true })
-
-esbuild.build({
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    outfile: './build/index.js',
-    platform: 'node',
-    target: 'esnext',
-    format: 'esm',
-    sourcemap: true,
-    tsconfig: './tsconfig.json',
-    external: [ './node_modules/*' ],
-}).catch(() => process.exit(1))
+build(pkgJson.dependencies)
